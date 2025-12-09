ee105c74ba4cfcedf18d66535b85b5f7ea6975c6
b3koslev <b3koslev@SZT1-19-HP>
2025-12-09 12:25:43 +0100

    House model input created

--- robot_vacuum_cleaner/Program.cs
+++ robot_vacuum_cleaner/Program.cs
@@ -8,8 +8,22 @@ namespace robot_vacuum_cleaner
 {
     internal class Program
     {
+        static void ModelGeneration()
+        {
+            int lines, columns;
+            do 
+            { 
+                Console.Write("Add meg a lakás sorainak számát (20, 30) intervallumban: ");
+                lines = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Add meg a lakás oszlopainak számát (20, 30) intervalumban: ");
+                columns = Convert.ToInt32(Console.ReadLine());
+            } while ((lines == columns) || (lines > 30 || lines < 20) ||(columns > 30 || columns < 20));
+
+            int[,] houseModel = new int[lines, columns];
+        }
         static void Main(string[] args)
         {
+            ModelGeneration();
         }
     }
 }
