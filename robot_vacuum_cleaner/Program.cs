8b9ea24a775b3d244f653c91168c8ded53b8e3e2
leventekoszo <leventekoszo33@gmail.com>
2026-01-05 16:58:38 +0100

    project finished

--- robot_vacuum_cleaner/Program.cs
+++ robot_vacuum_cleaner/Program.cs
@@ -125,6 +125,8 @@ namespace robot_vacuum_cleaner
         {
             Random steps = new Random();
             bool cleaned = false;
+            int cleaned_cells = 0;
+            int total_steps = 0;
             int dirty = 0;
             List<string> coordinates = new List<string>();
 
@@ -158,120 +160,74 @@ namespace robot_vacuum_cleaner
                     {
                         if (house[i, j] == "r")
                         {
-                            if (step_direction == "down")
+                            if ((house[i + 1, j] == "b") && (house[i - 1, j] == "b") && 
+                                (house[i, j + 1] == "b") && (house[i, j - 1] == "b") && 
+                                (i != house.GetLength(0)) && (j != house.GetLength(1)) && 
+                                (i != house.GetLength(0) - i) && (j != house.GetLength(1) - j))
                             {
-                                if (i == house.GetLength(0) - 1)
-                                {
-                                    Console.WriteLine("Robot falnak ütközött lefelé!");
-                                    continue;
-                                }
-                                else if (house[i + 1, j] == "b")
-                                {
-                                    Console.WriteLine("Robot bútornak ütközött lefelé!");
-                                    continue;
-                                }
-                                else if (house[i + 1, j] == "k")
-                                {
-                                    dirty++;
-                                    coordinates.Add($"{i + 1}, {j}");
-                                    house[i, j] = "-";
-                                    house[i + 1, j] = "r";
-                                    Console.WriteLine("A robot egyet lefelé lépett!");
-                                    continue;
-                                }
-                                else
-                                {
-                                    house[i, j] = "-";
-                                    house[i + 1, j] = "r";
-                                    Console.WriteLine("A robot egyet lefelé lépett!");
-                                    continue;
-                                }
+                                Console.WriteLine("A robot nem tud tovább haladni, mivel minden irányban bútor van!");
                             }
-                            else if (step_direction == "up")
+                            else
                             {
-                                if (i == 0)
+                                if (step_direction == "down")
                                 {
-                                    Console.WriteLine("Robot falnak ütközött felfelé!");
-                                    continue;
+                                    if (house[i + 1, j] == "k")
+                                    {
+                                        dirty++;
+                                        coordinates.Add($"{i + 1}, {j}");
+                                        total_steps++;
+                                        cleaned_cells++;
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
                                 }
-                                else if (house[i - 1, j] == "b")
+                                else if (step_direction == "up")
                                 {
-                                    Console.WriteLine("Robot bútornak ütközött felfelé!");
-                                    continue;
+                                    if (house[i - 1, j] == "k")
+                                    {
+                                        dirty++;
+                                        coordinates.Add($"{i - 1}, {j}");
+                                        total_steps++;
+                                        cleaned_cells++;
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
                                 }
-                                else if (house[i - 1, j] == "k")
+                                else if (step_direction == "right")
                                 {
-                                    dirty++;
-                                    coordinates.Add($"{i - 1}, {j}");
-                                    house[i, j] = "-";
-                                    house[i - 1, j] = "r";
-                                    Console.WriteLine("A robot egyet felfelé lépett!");
-                                    continue;
+                                    if (house[i, j + 1] == "k")
+                                    {
+                                        dirty++;
+                                        coordinates.Add($"{i}, {j + 1}");
+                                        total_steps++;
+                                        cleaned_cells++;
+                                        continue;
+                                    }
+                                    else 
+                                    {
+                                        continue;
+                                    }
                                 }
-                                else
+                                else if (step_direction == "left")
                                 {
-                                    house[i, j] = "-";
-                                    house[i - 1, j] = "r";
-                                    Console.WriteLine("A robot egyet felfelé lépett!");
-                                    continue;
-                                }
-                            }
-                            else if (step_direction == "right")
-                            {
-                                if (j == house.GetLength(1) - 1)
-                                {
-                                    Console.WriteLine("Robot falnak ütközött jobbra!");
-                                    continue;
-                                }
-                                else if (house[i, j + 1] == "b")
-                                {
-                                    Console.WriteLine("Robot bútornak ütközött jobbra!");
-                                    continue;
-                                }
-                                else if (house[i, j + 1] == "k")
-                                {
-                                    dirty++;
-                                    coordinates.Add($"{i}, {j + 1}");
-                                    house[i, j] = "-";
-                                    house[i, j + 1] = "r";
-                                    Console.WriteLine("A robot egyet jobbra lépett!");
-                                    continue;
-                                }
-                                else
-                                {
-                                    house[i, j] = "-";
-                                    house[i, j + 1] = "r";
-                                    Console.WriteLine("A robot egyet jobbra lépett!");
-                                    continue;
-                                }
-                            }
-                            else if (step_direction == "left")
-                            {
-                                if (j == 0)
-                                {
-                                    Console.WriteLine("Robot falnak ütközött balra!");
-                                    continue;
-                                }
-                                else if (house[i, j - 1] == "b")
-                                {
-                                    Console.WriteLine("Robot bútornak ütközött balra!");
-                                    continue;
-                                }
-                                else if (house[i, j - 1] == "k")
-                                {
-                                    dirty++;
-                                    coordinates.Add($"{i}, {j - 1}");
-                                    house[i, j] = "-";
-                                    house[i, j - 1] = "r";
-                                    Console.WriteLine("A robot egyet balra lépett!");
-                                    continue;
-                                }
-                                else
-                                {
-                                    house[i, j] = "-";
-                                    house[i, j - 1] = "r";
-                                    Console.WriteLine("A robot egyet balra lépett!");
-                                    continue;
+                                    if (house[i, j - 1] == "k")
+                                    {
+                                        dirty++;
+                                        coordinates.Add($"{i}, {j - 1}");
+                                        total_steps++;
+                                        cleaned_cells++;
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
                                 }
                             }
                         }
@@ -281,45 +237,14 @@ namespace robot_vacuum_cleaner
                         }
                     }
                 }
-
-                for (int i = 0; i < house.GetLength(0); i++)
-                {
-                    for (int j = 0; j < house.GetLength(1); j++)
-                    {
-                        switch (house[i, j])
-                        {
-                            case "-":
-                                Console.ForegroundColor = ConsoleColor.Yellow;
-                                Console.Write(house[i, j] + " ");
-                                break;
-                            case "b":
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.Write(house[i, j] + " ");
-                                break;
-                            case "k":
-                                Console.ForegroundColor = ConsoleColor.Blue;
-                                Console.Write(house[i, j] + " ");
-                                break;
-                            case "r":
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.Write(house[i, j] + " ");
-                                break;
-                        }
-                    }
-                    Console.WriteLine();
-                }
-
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine();
-                Console.WriteLine("A továbblépéshez nyomjon meg egy billentyűt!");
-                Console.ReadKey();
-                Console.Clear();
-
             } while (!cleaned);
 
             Console.WriteLine("===========================");
             Console.WriteLine("\tStatisztika");
             Console.WriteLine("===========================");
+
+            Console.WriteLine("Összes lépés: " + total_steps);
+            Console.WriteLine("Tisztított cellák száma: " + cleaned_cells);
 
             Console.WriteLine("Koszos cellák száma: " + dirty);
 
@@ -355,12 +280,13 @@ namespace robot_vacuum_cleaner
             string[,] houseModel = ModelGeneration();
             string[,] house = CreateModel(houseModel);
 
+            WriteHouse(house);
+
             bool cleaned = CheckCleaned(house);
-            do
+            if (cleaned)
             {
-                WriteHouse(house);
                 Cleaning(house);
-            } while (cleaned == false);
+            }
         }
     }
 }
